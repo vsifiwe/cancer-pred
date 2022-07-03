f58395a6ed3b708eb32d0f09ddac45ed4e608e54
asifiwe-manzi <asifiwe.manzi@amalitech.com>
2022-07-03 18:02:50 +0200

    initial commit

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, UsersSerializer
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "message": "registered successfully",
+        })
